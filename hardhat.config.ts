fcad08571d357e3b6fce82199da2816973f6aa9c
David Fraga <david.fraga610@hotmail.com>
2022-08-22 12:21:00 -0500

    chore: app skeleton

---
+++ hardhat.config.ts
@@ -0,0 +1,83 @@
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import 'hardhat-contract-sizer';
+import 'hardhat-docgen';
+import 'hardhat-exposed-exclude-vars';
+import 'hardhat-gas-reporter';
+import 'hardhat-packager';
+import 'hardhat-watcher';
+import { HardhatUserConfig } from 'hardhat/config';
+import 'tsconfig-paths/register';
+import 'solidity-coverage';
+
+export default <HardhatUserConfig>{
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.4',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+  exposed: {
+    excludeVars: ['_auctionByListing'],
+  },
+  networks: {
+    regtest: {
+      url: 'http://localhost:4444',
+    },
+  },
+  typechain: {
+    target: 'ethers-v5',
+    outDir: 'typechain-types',
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: false,
+  },
+  watcher: {
+    compilation: {
+      tasks: ['compile'],
+      files: ['./contracts'],
+      verbose: true,
+    },
+    tdd: {
+      tasks: [
+        // 'clean',
+        // { command: 'compile', params: { quiet: true } },
+        {
+          command: 'test',
+          params: {
+            noCompile: true,
+            testFiles: ['{path}'],
+          },
+        },
+      ],
+      files: ['./test/**/*.ts'],
+      verbose: true,
+    },
+  },
+  packager: {
+    includeFactories: false,
+    contracts: [],
+  },
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: false,
+  },
+};
+
+// export default config;
