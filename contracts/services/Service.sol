38ed3ad3ad5b091fd5eab7f8ca7687c400df0e01
Onyeka Ijeh <onyekaijeh@gmail.com>
2022-08-23 17:36:26 +0100

    feat: add lending service contracts

---
+++ contracts/services/Service.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract contract Service is Ownable {
+    enum ServiceType {
+        Borrowing,
+        Lending
+    }
+
+    ServiceType public  serviceType;
+}
+
