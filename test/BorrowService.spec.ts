7cd0a9851b775f69acd5d952cd0de26631e1cd0e
Anni Piragauta <anni.piragauta@iovlabs.org>
2022-08-23 12:09:15 -0500

    feat: defined tests

---
+++ test/BorrowService.spec.ts
@@ -0,0 +1,13 @@
+// import { expect } from 'chairc';
+// import { ethers } from 'hardhat';
+
+describe('BorrowService', () => {
+  it('should deployed a Borrow Service');
+  it('should return borrow service listings current liquidity');
+  it('should add liquidity to the borrow service listing');
+  it('should remove liquidity from the borrow service listing');
+  it('should add a new listing');
+  it('should remove a listing');
+  it('should return a listing');
+  it('should return borrow service count of a given currency');
+});
