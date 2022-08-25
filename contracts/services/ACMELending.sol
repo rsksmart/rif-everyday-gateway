efdcf18a82c346a4efcf24616b4f8c915751cee6
Diego Rosales <diego.rosales@iovlabs.org>
2022-08-25 08:40:45 -0600

    feat: initial acme lending contract

--- contracts/services/ACMELending.sol
+++ contracts/services/ACMELending.sol
@@ -4,42 +4,40 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ACMELending {
-  event Deposit(address indexed _from, uint256 _amount);
-  event Withdraw(address indexed _from, uint256 _amount);
+    event Deposit(address indexed _from, uint256 _amount);
+    event Withdraw(address indexed _from, uint256 _amount);
 
-  mapping(address => mapping(address => uint256)) private _balances;
-  uint256 private _subsidy = 0;
+    mapping(address => mapping(address => uint256)) private _balances;
+    uint256 private _subsidy = 0;
 
-  function deposit(address currency, uint256 amount) public payable {
-    require(msg.value > 0 || amount > 0, "No tokens sent");
+    function deposit(address currency, uint256 amount) public payable {
+        require(msg.value > 0 || amount > 0, "No tokens sent");
 
-    if (currency == address(0)) {
-      _balances[msg.sender][address(0)] += msg.value;
-    } else {
-      IERC20(currency).transferFrom(msg.sender, address(this), amount);
-      _balances[msg.sender][currency] += amount;
+        if (currency == address(0)) {
+            _balances[msg.sender][address(0)] += msg.value;
+        } else {
+            IERC20(currency).transferFrom(msg.sender, address(this), amount);
+            _balances[msg.sender][currency] += amount;
+        }
+
+        emit Deposit(msg.sender, amount);
     }
 
-    emit Deposit(msg.sender, amount);
-  }
+    function withdraw(address currency, uint256 amount) public {
+        require(_balances[msg.sender][currency] > 0, "Not enough balance");
 
-  function withdraw(address currency, uint256 amount) public {
-    require(_balances[msg.sender][currency] > 0, "Not enough balance");
-
-    if (currency == address(0)) {
-      _balances[msg.sender][address(0)] -= amount;
-      payable(msg.sender).transfer(amount);
-    } else {
-      _balances[msg.sender][currency] -= amount;
-      IERC20(currency).transferFrom(address(this), msg.sender, amount);
+        if (currency == address(0)) {
+            _balances[msg.sender][address(0)] -= amount;
+            payable(msg.sender).transfer(amount);
+        } else {
+            _balances[msg.sender][currency] -= amount;
+            IERC20(currency).transferFrom(address(this), msg.sender, amount);
+        }
     }
-  }
 
-  function getBalance(address currency) public view returns (uint256) {
-    return _balances[msg.sender][currency];
-  }
+    function getBalance(address currency) public view returns (uint256) {
+        return _balances[msg.sender][currency];
+    }
 
-  function _calculateInterest() private view {
-
-  }
+    function _calculateInterest() private view {}
 }
