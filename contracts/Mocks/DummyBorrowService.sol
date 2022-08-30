32a78e1020090444791a917af70dcee03c289011
Diego Rosales <diego.rosales@iovlabs.org>
2022-08-30 10:12:20 -0600

    feat: reorganize based on feedback

--- contracts/Mocks/DummyBorrowService.sol
+++ contracts/Mocks/DummyBorrowService.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import "./BorrowService.sol";
+import "../Services/BorrowService.sol";
 
 contract DummyBorrowService is BorrowService {
     function borrow(
