48bd7cabad3b01a99e6525b18a9ab4cdc696f411
Sergio <sergio.jara@iovlabs.org>
2022-09-01 17:13:36 -0300

    fix: addressed comments

--- scripts/wallet.ts
+++ scripts/wallet.ts
@@ -4,71 +4,12 @@ import {
   DummyLendingService,
   DummyLendingService__factory,
 } from 'typechain-types';
-import { deployContract, onlyDeployContract } from '../utils/deployment.utils';
 import moment, { duration } from 'moment';
 import { PaybackOption } from 'test/utils';
 import { oneRBTC } from 'test/mock.utils';
 import { ethers, network } from 'hardhat';
 import chalk from 'chalk';
 const rBTC = ethers.constants.AddressZero;
-
-// const setupLendingProtocol = async () => {
-//   // deploy the contract and get the deployed address
-//   const contract = await onlyDeployContract<DummyLendingService>(
-//     'DummyLendingService',
-//     {}
-//   );
-
-//   const rewardRate = oneRBTC.mul(10); //10%
-
-//   const listingTx = await contract.addListing(
-//     duration(60, 'days').asMilliseconds(),
-//     duration(3, 'years').asMilliseconds(),
-//     rBTC, //for now 👀
-//     PaybackOption.Month,
-//     rewardRate
-//   );
-
-//   const txReceipt = await listingTx.wait();
-
-//   const lendingServiceListingId = txReceipt.events?.find(
-//     (e: any) => e.event === 'LendingServiceAdded'
-//   )?.args?.lendingServiceId;
-
-//   return {
-//     lendingContract: contract,
-//     lendingServiceListingId,
-//   };
-// };
-
-// const setupBorrowingProtocol = async () => {
-//   // deploy the contract and get the deployed address
-//   const contract = await onlyDeployContract<DummyBorrowService>(
-//     'DummyBorrowService',
-//     {}
-//   );
-
-//   const listingTx = await contract.addListing({
-//     currency: rBTC,
-//     interestRate: 5,
-//     loanToValue: 10000,
-//     loanToValueTokenAddr: rBTC,
-//     maxAmount: 100,
-//     minAmount: 1,
-//     maxDuration: 1000,
-//   });
-
-//   const txReceipt = await listingTx.wait();
-
-//   const lendingServiceListingId = txReceipt.events?.find(
-//     (e: any) => e.event === 'LendingServiceAdded'
-//   )?.args?.lendingServiceId;
-
-//   return {
-//     lendingContract: contract,
-//     lendingServiceListingId,
-//   };
-// };
 
 async function deployDummyLendingServiceFixture() {
   const [owner] = await ethers.getSigners();
@@ -162,51 +103,5 @@ const executeLending = async () => {
     (await lendingContractAsLender.getBalance()).toString()
   );
 };
-
-// const executeBorrowing = async () => {
-//   const { lendingContract, lendingServiceListingId } =
-//     await setupLendingProtocol();
-
-//   const [owner, lender, ...otherUsers] = await ethers.getSigners();
-
-//   const lendingContractAsLender = lendingContract.connect(lender);
-
-//   const loanTx = await lendingContractAsLender.lend(
-//     oneRBTC.mul(10),
-//     rBTC,
-//     duration(3, 'months').asMilliseconds(),
-//     PaybackOption.Month,
-//     {
-//       value: oneRBTC.mul(10),
-//     }
-//   );
-
-//   await loanTx.wait();
-
-//   console.log(
-//     'Lender Balance on wallet',
-//     await ethers.provider.getBalance(lender.address)
-//   );
-//   console.log(
-//     'Lender Balance on lending contract',
-//     await lendingContract.getBalance(lender.address)
-//   );
-
-//   //time manipulation...
-
-//   const lenderCurrentBalance = await lendingContract.getBalance(lender.address);
-
-//   const withdrawTx = await lendingContract.withdraw(lenderCurrentBalance, rBTC);
-//   await withdrawTx.wait();
-
-//   console.log(
-//     'Lender Balance on wallet',
-//     await ethers.provider.getBalance(lender.address)
-//   );
-//   console.log(
-//     'Lender Balance on lending contract',
-//     await lendingContract.getBalance(lender.address)
-//   );
-// };
 
 executeLending().then(() => console.log('done 👀'));
