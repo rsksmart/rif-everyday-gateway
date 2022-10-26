db639bfff3392aa2e563e5d78642baadb76a26ff
Diego Rosales <diego.rosales@iovlabs.org>
2022-10-26 08:43:25 -0600

    feat: wip

--- contracts/smartwallet/SmartWallet.sol
+++ contracts/smartwallet/SmartWallet.sol
@@ -35,6 +35,21 @@ contract SmartWallet is IForwarder {
         );
     }
 
+    function getDomainSeparator() public view returns (bytes32) {
+        return
+            keccak256(
+                abi.encode(
+                    keccak256(
+                        "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                    ), //hex"8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f",
+                    keccak256("RSK RIF GATEWAY"), //DOMAIN_NAME hex"d41b7f69f4d7734774d21b5548d74704ad02f9f1545db63927a1d58479c576c8"
+                    DATA_VERSION_HASH,
+                    _getChainID(),
+                    address(this)
+                )
+            );
+    }
+
     function _setOwner(address owner) private {
         //To avoid re-entrancy attacks by external contracts, the first thing we do is set
         //the variable that controls "is initialized"
@@ -123,6 +138,21 @@ contract SmartWallet is IForwarder {
             ),
             "Signature mismatch"
         );
+    }
+
+    function recov(
+        bytes32 suffixData,
+        ForwardRequest memory req,
+        bytes memory sig
+    ) public view returns (address) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    domainSeparator,
+                    keccak256(_getEncoded(suffixData, req))
+                )
+            ).recover(sig);
     }
 
     function _getEncoded(bytes32 suffixData, ForwardRequest memory req)
---
+++ test/smartwallet/utils.ts
@@ -0,0 +1,130 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { ParamType } from 'ethers/lib/utils';
+import { ethers } from 'hardhat';
+import {
+  MessageTypeProperty,
+  MessageTypes,
+  signTypedData,
+  SignTypedDataVersion,
+  TypedMessage,
+  TypedDataUtils,
+} from '@metamask/eth-sig-util';
+import { IForwarder } from 'typechain-types';
+
+export const ONE_FIELD_IN_BYTES = 32;
+export const HARDHAT_CHAIN_ID = 31337;
+
+export const computeSalt = (
+  owner: SignerWithAddress,
+  factoryAddress: string
+): string => {
+  return ethers.utils.keccak256(
+    ethers.utils.solidityPack(
+      ['address', 'address', 'string'],
+      [owner.address, factoryAddress, '0']
+    )
+  );
+};
+
+export const encoder = (
+  types: readonly (string | ParamType)[],
+  values: readonly (string | ParamType)[]
+) => {
+  const encodedParams = ethers.utils.defaultAbiCoder.encode(types, values);
+  return encodedParams.slice(2);
+};
+
+interface Types extends MessageTypes {
+  EIP712Domain: MessageTypeProperty[];
+  ForwardRequest: MessageTypeProperty[];
+}
+
+type Domain = {
+  name: string;
+  version: string;
+  chainId: number;
+  verifyingContract: string;
+};
+
+export const domainSeparatorType = {
+  prefix: 'string name,string version',
+  name: 'RSK RIF GATEWAY',
+  version: '1',
+};
+
+export const eIP712DomainType: MessageTypeProperty[] = [
+  { name: 'name', type: 'string' },
+  { name: 'version', type: 'string' },
+  { name: 'chainId', type: 'uint256' },
+  { name: 'verifyingContract', type: 'address' },
+];
+
+export const forwardRequestType: MessageTypeProperty[] = [
+  { name: 'from', type: 'address' },
+  { name: 'nonce', type: 'uint256' },
+  { name: 'executor', type: 'address' },
+];
+
+export const getDomainSeparator = (
+  verifyingContract: string,
+  chainId: number
+): Domain => {
+  return {
+    name: domainSeparatorType.name,
+    version: domainSeparatorType.version,
+    chainId: chainId,
+    verifyingContract: verifyingContract,
+  };
+};
+
+export function getLocalEip712Signature(
+  typedRequestData: TypedMessage<Types>,
+  privateKey: Buffer
+): string {
+  return signTypedData({
+    privateKey: privateKey,
+    data: typedRequestData,
+    version: SignTypedDataVersion.V4,
+  });
+}
+
+export class TypedRequestData implements TypedMessage<Types> {
+  readonly types: Types;
+
+  readonly domain: Domain;
+
+  readonly primaryType: string;
+
+  readonly message: Record<string, unknown>;
+
+  constructor(
+    chainId: number,
+    verifier: string,
+    forwardRequest: IForwarder.ForwardRequestStruct
+  ) {
+    this.types = {
+      EIP712Domain: eIP712DomainType,
+      ForwardRequest: forwardRequestType,
+    };
+    this.domain = getDomainSeparator(verifier, chainId);
+    this.primaryType = 'ForwardRequest';
+    // in the signature, all "request" fields are flattened out at the top structure.
+    // other params are inside "relayData" sub-type
+    this.message = {
+      ...forwardRequest,
+    };
+  }
+}
+
+export const getSuffixData = (typedRequestData: TypedRequestData): string => {
+  const encoded = TypedDataUtils.encodeData(
+    typedRequestData.primaryType,
+    typedRequestData.message,
+    typedRequestData.types,
+    SignTypedDataVersion.V4
+  );
+
+  const messageSize = Object.keys(typedRequestData.message).length;
+
+  return '0x' + encoded.slice(messageSize * ONE_FIELD_IN_BYTES).toString('hex');
+};
