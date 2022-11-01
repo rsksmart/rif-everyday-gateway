42b3f76f6ba74386c07f09180a62abd3d7ff5eca
Maduabuchi Chinaechetam <cmaduabuchi@live.com>
2022-11-01 15:31:17 +0100

    Remove Whitelisting for providers (#35)
    * feat: remove providers whitelisting
    
    * refactor: remove providers whitelisting approach
    
    * refactor: remove providers whitelisting approach
    
    Co-authored-by: Onyeka Ijeh <onyekaijeh@gmail.com>

--- contracts/services/Providers.sol
+++ contracts/services/Providers.sol
@@ -11,9 +11,7 @@ contract Providers is Ownable {
     error NonConformity(string NonConformityErrMsg);
 
     mapping(address => Service[]) private _servicesByProvider;
-    mapping(address => Service[]) private _pendingServicesByProvider;
     address[] private _providers;
-    address[] private _pendingProviders;
     uint256 private _totalServices;
     ServiceTypeManager private _serviceTypeManager;
 
@@ -37,23 +35,12 @@ contract Providers is Ownable {
             );
         }
 
+        // Proceeds to add the provider and service
         address provider = service.owner();
         if (provider == address(0)) revert InvalidProviderAddress(provider);
-        if (!_isOnAddressArray(_pendingProviders, provider))
-            _pendingProviders.push(provider);
-        _pendingServicesByProvider[provider].push(service);
+        _providers.push(provider);
+        _servicesByProvider[provider].push(service);
         _totalServices++;
-    }
-
-    function validate(bool approved, Service service) external onlyOwner {
-        address provider = service.owner();
-        if (approved) {
-            if (!_isOnAddressArray(_providers, provider)) {
-                _providers.push(provider);
-            }
-            _servicesByProvider[provider].push(service);
-        }
-        //TODO: else clean _pendingProviders & _pendingServices
     }
 
     function getServices() external view returns (Service[] memory) {
@@ -69,16 +56,5 @@ contract Providers is Ownable {
             }
         }
         return allServices;
-    }
-
-    function _isOnAddressArray(address[] memory arr, address add)
-        internal
-        pure
-        returns (bool)
-    {
-        for (uint256 i = 0; i < arr.length; i++) {
-            if (arr[i] == add) return true;
-        }
-        return false;
     }
 }
