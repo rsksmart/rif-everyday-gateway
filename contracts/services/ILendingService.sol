3f171c7c3662dbcfd13fe484c6c0d78cf8fe5dd4
Diego Rosales <diego.rosales@iovlabs.org>
2022-11-07 12:19:08 -0600

    feat: update imports

--- contracts/services/ILendingService.sol
+++ contracts/services/ILendingService.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
+
 import "./IService.sol";
 import "../smartwallet/IForwarder.sol";
 
 interface ILendingService is IService {
+    event Lend(
+        uint256 indexed listingId,
+        address indexed lender,
+        address indexed currency,
+        uint256 amount
+    );
+
     function lend(
         bytes32 suffixData,
         IForwarder.ForwardRequest memory req,
