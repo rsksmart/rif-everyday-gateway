66ba1e9c690a316d392440d19bffe174b40c6404
Diego Rosales <diego.rosales@iovlabs.org>
2023-01-16 16:38:08 -0600

    fix: solve high severity slither issues

--- contracts/services/ServiceTypeManager.sol
+++ contracts/services/ServiceTypeManager.sol
@@ -12,23 +12,6 @@ import "./IService.sol";
  * @author RIF protocols team
  */
 contract ServiceTypeManager is Ownable, ERC165Storage {
-    mapping(bytes4 => bool) private _supportedInterfaces;
-
-    /**
-     * @inheritdoc IERC165
-     */
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override
-        returns (bool)
-    {
-        return
-            super.supportsInterface(interfaceId) ||
-            _supportedInterfaces[interfaceId];
-    }
-
     /**
      * @notice Allows owner to register a new service type
      * @param newServiceInterfaceId The id of the interface
