80db0ecd0b2c847f1db02c0d5bb6f437fcba9567
Anni Piragauta <anni.piragauta@iovlabs.org>
2023-01-18 16:57:54 -0500

    docs: smart wallet

--- contracts/smartwallet/ISmartWalletFactory.sol
+++ contracts/smartwallet/ISmartWalletFactory.sol
@@ -4,15 +4,41 @@ pragma experimental ABIEncoderV2;
 
 import "./SmartWallet.sol";
 
+/**
+ * @title ISmartWalletFactory
+ * @dev Factory interface for creating SmartWallets
+ * Based on rif-relay-contracts interfaces/SmartWalletFactory.sol
+ * @author RIF protocols team
+ */
 interface ISmartWalletFactory {
-    event Deployed(address indexed addr, uint256 salt); //Event triggered when a deploy is successful
+    /**
+     * @notice Emitted when a new SmartWallet is deployed successfully
+     * @param addr The address of the SmartWallet
+     * @param salt The salt used to deploy the SmartWallet
+     */
+    event Deployed(address indexed addr, uint256 salt);
 
+    /**
+     * @notice Deploys a new SmartWallet for a user
+     * @param owner The address of the owner of the SmartWallet
+     */
     function createUserSmartWallet(address owner) external;
 
+    /**
+     * @notice Returns the calculated address of a SmartWallet for a user EOA without deploying it
+     * @param owner The address of the owner EOA of the SmartWallet
+     * @return smartWalletAddress The address of the SmartWallet of the owner
+     */
     function getSmartWalletAddress(address owner)
         external
         view
         returns (address);
 
+    /**
+     * @notice Returns the SmartWallet for a user, if it has not been deployed deploys it first
+     * and then returns the SmartWallet
+     * @param owner The address of the owner of the SmartWallet
+     * @return smartWallet The SmartWallet of the owner
+     */
     function getSmartWallet(address owner) external returns (SmartWallet);
 }
