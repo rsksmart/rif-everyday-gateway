7b93d1c79704fd1a8db9c5968da41b5efaa21038
Anni Piragauta <anni.piragauta@iovlabs.org>
2023-01-18 17:44:36 -0500

    docs: mocks

--- contracts/mocks/ACME.sol
+++ contracts/mocks/ACME.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.16;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title ACME
+ * @dev ACME is a mock o f a service provider of lending
+ * and borrowing services for testing purposes.
+ * Inspired on Compound protocol
+ * @author RIF protocols team
+ */
 contract ACME is Ownable {
     error InvalidAmount(uint256 amount);
     error NotEnoughBalance(uint256 amount);
@@ -38,6 +45,11 @@ contract ACME is Ownable {
 
     mapping(address => uint256) private _collateralFactors;
 
+    /**
+     * @notice allows ACME owner to update currencies collateral factors
+     * @param currency the currency address
+     * @param factor the new collateral factor for the currency
+     */
     function updateCollateralFactor(address currency, uint256 factor)
         external
         onlyOwner
@@ -50,14 +62,26 @@ contract ACME is Ownable {
         emit ReceivedLiquidity(msg.value);
     }
 
+    /**
+     * @notice Deposits the msg.value from msg.sender to the ACME protocol
+     */
     function deposit() external payable {
         _deposit(msg.value, msg.sender);
     }
 
+    /**
+     * @notice Deposits the msg.value from the depositor to the ACME protocol
+     * @param depositor the address of the depositor
+     */
     function deposit(address depositor) external payable {
         _deposit(msg.value, depositor);
     }
 
+    /**
+     * @notice Deposits RBTC from the depositor to the ACME protocol
+     * @param amount the amount of funds to deposit
+     * @param depositor the address of the depositor
+     */
     function _deposit(uint256 amount, address depositor) internal {
         if (amount == 0) {
             revert InvalidAmount(amount);
@@ -72,14 +96,62 @@ contract ACME is Ownable {
         emit Deposit(depositor, amount);
     }
 
+    /**
+     * @notice Allows the msg.sender to withdraw the deposit plus interest
+     * @param amount The amount of RBTC to withdraw
+     */
     function withdraw(uint256 amount) external {
         _withdraw(amount, msg.sender);
     }
 
+    /**
+     * @notice Allows the withdrawer to withdraw the deposit plus interest
+     * @param amount The amount of RBTC to withdraw
+     * @param withdrawer The address that will receive the RBTC
+     */
     function withdraw(uint256 amount, address withdrawer) external {
         _withdraw(amount, withdrawer);
     }
 
+    /**
+     * @notice Allows the withdrawer to withdraw the deposit plus interest
+     * @param amount The amount of RBTC to withdraw
+     * @param withdrawer The address that will receive the RBTC
+     */
+    function _withdraw(uint256 amount, address withdrawer) internal {
+        require(
+            !_balances[withdrawer][address(0)].locked,
+            "balance is collateral"
+        );
+        if (
+            _balances[withdrawer][address(0)].amount == 0 ||
+            amount > _balances[withdrawer][address(0)].amount
+        ) {
+            revert NotEnoughBalance(amount);
+        }
+
+        uint256 interest = _calculateInterest(withdrawer, address(0));
+
+        _balances[withdrawer][address(0)].amount -= amount;
+
+        if (_balances[withdrawer][address(0)].amount == 0) {
+            delete _balances[withdrawer][address(0)];
+        }
+
+        uint256 total = amount + interest;
+
+        (bool sent, ) = payable(withdrawer).call{value: total}("");
+        require(sent, "failed to send value");
+
+        emit Withdraw(withdrawer, total);
+    }
+
+    /**
+     * @notice Allows the loaner to borrow a certain amount of tokens
+     * @param currency The address of the token to borrow
+     * @param amount The amount of tokens to borrow
+     * @param loaner The address that will receive the tokens
+     */
     function loan(
         address currency,
         uint256 amount,
@@ -88,10 +160,21 @@ contract ACME is Ownable {
         _loan(currency, amount, loaner);
     }
 
+    /**
+     * @notice Allows the msg.sender to borrow a certain amount of tokens
+     * @param currency The address of the token to borrow
+     * @param amount The amount of tokens to borrow
+     */
     function loan(address currency, uint256 amount) external {
         _loan(currency, amount, msg.sender);
     }
 
+    /**
+     * @notice Allows the loaner to borrow a certain amount of tokens
+     * @param currency The address of the token to borrow
+     * @param amount The amount of tokens to borrow
+     * @param loaner The address that will receive the tokens
+     */
     function _loan(
         address currency,
         uint256 amount,
@@ -122,6 +205,12 @@ contract ACME is Ownable {
         emit Loan(loaner, amount, currency);
     }
 
+    /**
+     * @notice Allows the msg.sender to repay the debt of the loaner plus interest
+     * @param currency The currency of the loan
+     * @param amount The amount of currency to repay
+     * @param loaner The address of the debt creditor
+     */
     function repay(
         address currency,
         uint256 amount,
@@ -130,6 +219,13 @@ contract ACME is Ownable {
         _repay(currency, amount, msg.sender, loaner);
     }
 
+    /**
+     * @notice Allows the payer to repay the debt of the loaner plus interest
+     * @param currency The currency of the loan
+     * @param amount The amount of currency to repay
+     * @param payer The address that will pay the debt of the loaner the currency
+     * @param loaner The address of the debt creditor
+     */
     function repay(
         address currency,
         uint256 amount,
@@ -139,6 +235,13 @@ contract ACME is Ownable {
         _repay(currency, amount, payer, loaner);
     }
 
+    /**
+     * @notice Allows the payer to repay the debt of the loaner plus interest
+     * @param currency The currency of the loan
+     * @param amount The amount of currency to repay
+     * @param payer The address that will pay the debt of the loaner the currency
+     * @param loaner The address of the debt creditor
+     */
     function _repay(
         address currency,
         uint256 amount,
@@ -164,34 +267,12 @@ contract ACME is Ownable {
         emit Repay(loaner, amount, currency);
     }
 
-    function _withdraw(uint256 amount, address withdrawer) internal {
-        require(
-            !_balances[withdrawer][address(0)].locked,
-            "balance is collateral"
-        );
-        if (
-            _balances[withdrawer][address(0)].amount == 0 ||
-            amount > _balances[withdrawer][address(0)].amount
-        ) {
-            revert NotEnoughBalance(amount);
-        }
-
-        uint256 interest = _calculateInterest(withdrawer, address(0));
-
-        _balances[withdrawer][address(0)].amount -= amount;
-
-        if (_balances[withdrawer][address(0)].amount == 0) {
-            delete _balances[withdrawer][address(0)];
-        }
-
-        uint256 total = amount + interest;
-
-        (bool sent, ) = payable(withdrawer).call{value: total}("");
-        require(sent, "failed to send value");
-
-        emit Withdraw(withdrawer, total);
-    }
-
+    /**
+     * @notice Get the balance of a depositor on ETH
+     * @param depositor The address of the depositor
+     * @return deposited The amount deposited by the user
+     * @return interest The interest accrued from the lending
+     */
     function getBalance(address depositor)
         external
         view
@@ -200,6 +281,11 @@ contract ACME is Ownable {
         return _getBalance(depositor);
     }
 
+    /**
+     * @notice Get the balance of a msg.sender on ETH
+     * @return deposited The amount deposited by the user
+     * @return interest The interest accrued from the lending
+     */
     function getBalance()
         external
         view
@@ -208,6 +294,12 @@ contract ACME is Ownable {
         return _getBalance(msg.sender);
     }
 
+    /**
+     * @notice Get the balance of a depositor on ETH
+     * @param depositor The address of the depositor
+     * @return deposited The amount deposited by the user
+     * @return interest The interest accrued from the lending
+     */
     function _getBalance(address depositor)
         internal
         view
@@ -219,6 +311,12 @@ contract ACME is Ownable {
         );
     }
 
+    /**
+     * @notice Calculates simple interest accrued since last checkpoint
+     * @param from address of the user
+     * @param currency address of the currency
+     * @return interest accrued
+     */
     function _calculateInterest(address from, address currency)
         private
         view
@@ -238,6 +336,11 @@ contract ACME is Ownable {
                 _interestPer100Blocks) / (100 * 100);
     }
 
+    /**
+     * @notice Returns the collateral factor on a currency
+     * @param currency The currency to get the collateral factor
+     * @return The collateral factor of the given currency
+     */
     function getCetCollateralFactor(address currency)
         external
         view
@@ -246,10 +349,21 @@ contract ACME is Ownable {
         return _collateralFactors[currency];
     }
 
+    /**
+     * @notice Get the debt balance of the msg.sender on a given currency
+     * @param currency The currency of the debt
+     * @return The debt balance of the msg.sender on a given currency
+     */
     function getDebtBalance(address currency) external view returns (uint256) {
         return _getDebtBalance(currency, msg.sender);
     }
 
+    /**
+     * @notice Get the debt balance of a loaner on a given currency
+     * @param currency The currency of the debt
+     * @param loaner The address of the loaner
+     * @return The debt balance of the loaner on a given currency
+     */
     function getDebtBalance(address currency, address loaner)
         external
         view
@@ -258,6 +372,12 @@ contract ACME is Ownable {
         return _getDebtBalance(currency, loaner);
     }
 
+    /**
+     * @notice Get the debt balance of a loaner on a given currency
+     * @param currency The currency of the debt
+     * @param loaner The address of the loaner
+     * @return The debt balance of the loaner on a given currency
+     */
     function _getDebtBalance(address currency, address loaner)
         internal
         view
