8e27715754fcc287aeb86a419b21c9b0409a998d
Anni Piragauta <annipi@users.noreply.github.com>
2023-01-19 17:35:39 -0500

    Update contracts/gateway/IRIFGateway.sol
    Co-authored-by: Diego Rosales <diego.rosales@iovlabs.org>

--- contracts/gateway/IRIFGateway.sol
+++ contracts/gateway/IRIFGateway.sol
@@ -51,7 +51,7 @@ interface IRIFGateway {
     /**
      * @notice Returns services and providers registered on the gateway
      * @return services array of services registered on the gateway
-     * @return providers array of providers registered at the gateway
+     * @return providers array of providers registered on the gateway
      */
     function getServicesAndProviders()
         external
