17f0fae5fae35c19d0a35ef45ac2550bb189e05e
Anni Piragauta <anni.piragauta@iovlabs.org>
2023-02-10 16:32:14 -0500

    docs: adding missing docs IOwnable

--- contracts/access/IOwnable.sol
+++ contracts/access/IOwnable.sol
@@ -6,7 +6,16 @@ pragma solidity ^0.8.16;
  * @notice Interface for the Ownable contract.
  */
 interface IOwnable {
+    /**
+     * @notice Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     * @param newOwner The address of the new owner.
+     */
     function transferOwnership(address newOwner) external;
 
+    /**
+     * @notice Returns the address of the current owner.
+     * @return The address of the current owner.
+     */
     function owner() external view returns (address);
 }
